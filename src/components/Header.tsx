f51429c70a1757f57a323cb8428ec93d44bbf671
dhoonjang <dhoonjang@gmail.com>
2023-12-02 21:39:21 +0900

    Sidebar 컴포넌트 및 posts Dynamic Route 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,29 @@
 import Link from 'next/link';
-import { FC } from 'react';
-import { AiOutlineMenu } from 'react-icons/ai';
+import { Dispatch, FC, SetStateAction } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import { BsRobot } from 'react-icons/bs';
 
-const Header: FC = () => {
+type HeaderProps = {
+  isSidebarOpen: boolean;
+  setIsSidebarOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+const Header: FC<HeaderProps> = ({ isSidebarOpen, setIsSidebarOpen }) => {
   return (
     <header className="flex h-16 items-center justify-between border-b px-4 lg:h-20 lg:px-10">
-      <button className="p-2">
-        <AiOutlineMenu className="h-5 w-5 lg:h-6 lg:w-6" />
+      <button onClick={() => setIsSidebarOpen((t) => !t)} className="p-2">
+        {isSidebarOpen ? (
+          <AiOutlineClose className="h-5 w-5 lg:h-6 lg:w-6" />
+        ) : (
+          <AiOutlineMenu className="h-5 w-5 lg:h-6 lg:w-6" />
+        )}
       </button>
       <Link href="/">
         <h1 className="text-3xl font-medium text-slate-600 lg:text-4xl">
           BLOG
         </h1>
       </Link>
-      <Link href="/posts" className="p-2">
+      <Link href="/search" className="p-2">
         <BsRobot className="h-5 w-5 lg:h-6 lg:w-6" />
       </Link>
     </header>
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,49 @@
+import { cn } from '@/utils/style';
+import Link from 'next/link';
+import { FC } from 'react';
+import { AiFillGithub, AiFillInstagram, AiOutlineClose } from 'react-icons/ai';
+
+type SidebarProps = {
+  close: () => void;
+  isOpen: boolean;
+};
+
+const Sidebar: FC<SidebarProps> = ({ close, isOpen }) => {
+  return (
+    <div
+      className={cn(
+        'absolute min-h-screen flex-col gap-6 border-r bg-white p-10 pr-6 text-base lg:relative',
+        isOpen ? 'flex' : 'hidden',
+      )}
+    >
+      <div className="flex justify-end lg:hidden">
+        <AiOutlineClose className="h-5 w-5" onClick={close} />
+      </div>
+      <Link href="/" className="w-48 font-medium text-gray-600 hover:underline">
+        홈
+      </Link>
+      <Link
+        href="/tag"
+        className="w-48 font-medium text-gray-600 hover:underline"
+      >
+        태그
+      </Link>
+      <Link
+        href="/category/Web-Development"
+        className="w-48 font-medium text-gray-600 hover:underline"
+      >
+        Web Development
+      </Link>
+      <div className="mt-10 flex items-center gap-4">
+        <Link href="https://www.instagram.com/dhoonjang" target="_blank">
+          <AiFillInstagram className="h-6 w-6" />
+        </Link>
+        <Link href="https://www.github.com/dhoonjang" target="_blank">
+          <AiFillGithub className="h-6 w-6" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
